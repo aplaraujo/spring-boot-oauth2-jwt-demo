5d7a7d470526a4a988967f858b78c5f7d2b626a9
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-09-09 19:28:28 -0300

    BCrypt password encoder

--- password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-4-3-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$XZSpL4uVu.22SfykahYfuOcAidE6W9Nuo8VFjef62H50k0J7AqIpG');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$XZSpL4uVu.22SfykahYfuOcAidE6W9Nuo8VFjef62H50k0J7AqIpG');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
