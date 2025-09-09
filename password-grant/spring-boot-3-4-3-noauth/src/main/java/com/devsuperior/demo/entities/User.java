57e5dfa85c6d21dc9f15cbaad764b92fcc85af8f
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-09-09 19:53:55 -0300

    Spring Security checklist part 1

--- password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/entities/User.java
+++ password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/entities/User.java
@@ -1,9 +1,13 @@
 package com.devsuperior.demo.entities;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -17,7 +21,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -66,6 +70,7 @@ public class User {
         this.email = email;
     }
 
+    @Override
     public String getPassword() {
         return password;
     }
@@ -102,5 +107,15 @@ public class User {
     @Override
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
     }
 }
